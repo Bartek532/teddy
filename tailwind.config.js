a5917a99485a9177536ae97a4cf682d633294ebb
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-05-05 18:41:26 +0200

    feat: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      background: {
+        100: "#F7f1f3",
+      },
+      white: {
+        100: "#ffffff",
+        200: "#f0f6fc",
+      },
+      gray: {
+        100: "#ece8eb",
+        200: "#ece8eb",
+        300: "#b5b8b9",
+        400: "#fcfcfc",
+        500: "#ece8eb",
       },
     },
+    fontFamily: {
+      sans: ["var(--font-walsheim)", ...defaultTheme.fontFamily.sans],
+      mono: ["var(--font-league-mono)", ...defaultTheme.fontFamily.mono],
+      serif: ["var(--font-kenfolg)", ...defaultTheme.fontFamily.serif],
+    },
+    extend: {},
   },
   plugins: [],
-}
+};
