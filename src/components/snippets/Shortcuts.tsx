4a18784ed2100f3db48d7da6065f3cbf372ce812
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-05-06 22:33:04 +0200

    feat: add edit button to snippet

--- src/components/snippets/Shortcuts.tsx
+++ src/components/snippets/Shortcuts.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 
 import BinIcon from "@/public/svg/bin.svg";
+import PencilICon from "@/public/svg/pencil.svg";
 import { useSnippetsContext } from "@/src/providers/SnippetsProvider";
 
 import { Icon } from "../Icon";
@@ -20,7 +21,7 @@ export const Shortcuts = memo<ShortcutsProps>(({ snippets }) => {
         return (
           <li
             key={snippet.id}
-            className="grid grid-cols-[2fr_2fr_0.5fr] gap-2 items-stretch"
+            className="grid grid-cols-[2fr_2fr_0.4fr_0.4fr] gap-2 items-stretch"
           >
             <div
               className="flex justify-center items-center border-2 border-solid rounded-2xl p-2.5 gap-3.5"
@@ -33,6 +34,13 @@ export const Shortcuts = memo<ShortcutsProps>(({ snippets }) => {
             <Input className="h-full text-center">
               <span className="sr-only">Shortcut</span>
             </Input>
+            <button
+              className="group border-2 border-blue-100 bg-blue-100 transition hover:bg-transparent rounded-2xl flex items-center justify-center"
+              onClick={() => removeSnippet(snippet.id)}
+            >
+              <span className="sr-only">Edit</span>
+              <PencilICon className="w-5 fill-white-100 group-hover:fill-blue-100" />
+            </button>
             <button
               className="group border-2 border-red-100 bg-red-100 transition hover:bg-transparent rounded-2xl flex items-center justify-center"
               onClick={() => removeSnippet(snippet.id)}
